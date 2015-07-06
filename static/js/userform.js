23f250ce25a41fd75dbc8e250a9ecfee5f98eaf3
kyer <grzpjdyc1653@126.com>
2015-07-06 21:36:37 +0800

    change userform ajax submit

--- static/js/userform.js
+++ static/js/userform.js
@@ -26,24 +26,22 @@ $(function() {
                 cache: false,
                 success: function() {
                     // Enable button & show success message
-                    $("#btnSubmit").attr("disabled", false);
                     $('#success').html("<div class='alert alert-success'>");
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>登录成功. </strong>");
+                        .append("<strong>登录成功！</strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
-                    //clear all fields
-                    $('#userForm').trigger("reset");
+                    setTimeout('window.location = "index.html"', 1000);
                 },
                 error: function() {
                     // Fail message
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>登录失败!</strong>");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#userForm').trigger("reset");
