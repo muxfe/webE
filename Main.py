9f9c0626b6b9a7f090c4cf36835cac847f0c159b
BencoLee <alexmao930918@gmail.com>
2015-07-09 15:52:36 +0800

    Done 2

--- Main.py
+++ Main.py
@@ -92,14 +92,6 @@ class ReadNoteBookHandler(BaseHandler):
     def get(self,notebook_name):
         username = self.get_current_user().decode()
         show_note = self.note.find({'username':username,'notebook_name':notebook_name})
-        # notebookid = self.notebook.find_one({'username':username})['notebookid']
-        # doc = self.note.find({'notebookid':notebookid,'username':username})
-        # notebook_name = 'Default'
-        # show_note = []
-        # for _ in doc:
-        #     show_note.append(_)
-        #     if notebook_name == 'Default':
-        #         notebook_name = _['notebook_name']
         self.render(
             'notebook.html',
             show_note = show_note,
@@ -126,7 +118,7 @@ class CreateNoteHandler(BaseHandler):
     @authenticated
     def get(self, notebook_name):
         username = self.get_current_user().decode()
-        notebookid = self.notebook.find_one({'username':username,'notebook_name':notebook_name})
+        notebookid = self.notebook.find_one({'username':username,'notebook_name':notebook_name})['notebookid']
         # notebook= self.notebook.find_one({'username':username})
         # notebook_name = notebook.get('notebook_name','Default') if notebook else 'Default'
         # notebookid = notebook['notebookid']
@@ -141,17 +133,25 @@ class CreateNoteHandler(BaseHandler):
         title = self.get_argument('title')
         content = self.get_argument('content')
         notebookid = self.get_argument('notebookid')
-        notebook = self.notebook.find_one({'notebookid':notebookid})
+        # print(title,content,notebookid)
+        # notebook = self.notebook.find_one({'notebookid':notebookid})
         username = self.get_current_user().decode()
         notebook_name = notebook_name
-        count = self.note.count() + 1
+        # count = self.note.count() + 1
+        counts = 1
+        noteids = self.note.find({}).sort('noteid',pymongo.DESCENDING)
+        if noteids:
+            for _ in noteids:
+                counts = int(_['noteid']) + 1
+                break
+            # counts = int(self.note.find({}).sort('noteid',pymongo.DESCENDING)[0]['noteid']) + 1
         judge = self.note.find_one({'username':username,'note_title':title,'notebook_name':notebook_name})
         if judge == None:
             doc = {
                 'note_title' : title,
                 'note_content' : content,
                 'notebookid' : notebookid,
-                'noteid' : str(count),
+                'noteid' : str(counts),
                 'notebook_name' : notebook_name,
                 'username' : self.get_current_user().decode(),
                 'create_date' : datetime.now().strftime('%Y-%m-%d %I:%M:%S %p'),
@@ -180,13 +180,19 @@ class FuckNoteBookHandler(BaseHandler):
     def post(self, username):
         notebook_name = self.get_argument('notebook_name')
         notebook_description = self.get_argument('notebook_description')
-        count = self.notebook.count() + 1
+        # count = self.notebook.count() + 1
+        counts= 1
+        notebookids = self.notebook.find({}).sort('notebookid',pymongo.DESCENDING)
+        if notebookids:
+            for _ in notebookids:
+                counts = int(_['notebookid']) + 1
+                break
         judge = self.notebook.find_one({'username':username,'notebookname':notebook_name})
         userid = self.user.find_one({'username':username})['userid']
-        # print("CreateNoteBook : %d %s %s",count,notebook_name,notebook_description)
+        # print("CreateNoteBook : %d %s %s"%(counts,notebook_name,notebook_description))
         if judge == None:
             doc = {
-                'notebookid' : str(count),
+                'notebookid' : str(counts),
                 'userid':userid,
                 'notebook_name' : notebook_name,
                 'notebook_description' : notebook_description,
@@ -197,10 +203,49 @@ class FuckNoteBookHandler(BaseHandler):
             self.notebook.insert(doc)
             self.write(json.dumps({'ok' : True}))
 
+# 删除笔记本,同时把笔记本中的笔记全部删除 /notebook/delete/notebook_name
+class DelNoteBookHandler(BaseHandler):
+    @authenticated
+    def get(self,notebook_name):
+        username = self.get_current_user().decode()
+        userid = self.user.find_one({'username':username})['userid']
+        notebookid = self.notebook.find_one_and_delete({'userid':userid,'username':username,'notebook_name':notebook_name})['notebookid']
+        self.note.remove({'notebook_name':notebook_name,'notebookid':notebookid})
+        self.redirect('/notebook_list/%s'%userid)
+
+# 删除笔记 /note/delete/noteid
+class DelNoteHandler(BaseHandler):
+    @authenticated
+    def get(self,noteid):
+        username = self.get_current_user().decode()
+        userid = self.user.find_one({'username':username})['userid']
+        # notes = self.note.find()
+        # for _ in notes:
+        #     print("noteid",_['noteid'])
+        #     print("note_title",_['note_title'])
+        #     print("note_content",_['note_content'])
+        #     print("notebookid",_['notebookid'])
+        #     print("notebook_name",_['notebook_name'])
+        #     print("username",_['username'])
+        #     print("create_date",_['create_date'])
+        #     print("change_date",_['change_date'])
+        #     print("visit",_['visit'])
+        # print(userid,username,noteid)
+        note = self.note.remove({'username':username,'noteid':noteid})
+        print(note)
+        # print(notebook['username'])
+        # notebookid = notebook['notebookid']
+        # self.note.delete_many({'username':username,'userid':userid,'notebook_name':notebook_name,'notebookid':notebookid})
+        self.redirect('/notebook_list/%s'%userid)
+
 class LogoutHandler(BaseHandler):
     def get(self, *args, **kwargs):
         self.set_secure_cookie('username','')
         self.redirect('/')
+
+class PageNoteFoundHandler(BaseHandler):
+    def get(self):
+        raise tornado.web.HTTPError(404)
 
 class Application(tornado.web.Application):
     def __init__(self):
@@ -213,7 +258,10 @@ class Application(tornado.web.Application):
             (r'/notebook_list/([0-9a-zA-Z]+)', CreateNoteBookHandler),
             (r'/notebook/create/([0-9a-zA-Z]+)', FuckNoteBookHandler),
             (r'/notebook/([0-9a-zA-Z]+)', ReadNoteBookHandler),
+            (r'/notebook/delete/([0-9a-zA-Z]+)', DelNoteBookHandler),
+            (r'/note/delete/([0-9a-zA-Z]+)',DelNoteHandler),
             (r'/logout', LogoutHandler),
+            (r'.*',PageNoteFoundHandler),
         ]
         settings = dict(
             static_path = os.path.join(os.path.dirname('__file__'), 'static'),
@@ -228,6 +276,7 @@ class Application(tornado.web.Application):
 if __name__ == '__main__':
     tornado.options.parse_command_line()
     application = Application()
+    # tornado.web.ErrorHandler = PageNoteFoundHandler
     http_server = tornado.httpserver.HTTPServer(application)
     http_server.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
